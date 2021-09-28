88d7c5f5156040de9bdd88bd7eef621427e4a8cc
Sherab Pereira <sherabpereira@gmail.com>
2021-09-28 12:51:38 +0200

    FT: Basic JWT Authorization client side structure implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import './App.css';
-import {Switch, Route } from "react-router-dom";
-import HomePage from './pages/HomePage/HomePage';
-import AboutUsPage from './pages/AboutUsPage/AboutUsPage';
+import { Switch, Route } from 'react-router-dom'
 
+// import './App.css';
+
+import HomePage from './pages/HomePage/HomePage'
+import AboutUsPage from './pages/AboutUsPage/AboutUsPage'
+import SignupPage from './pages/MyAccount/SignupPage'
+import LoginPage from './pages/MyAccount/LoginPage'
+import AnonRoute from './components/AnonRoute/AnonRoute'
+import PrivateRoute from './components/PrivateRoute/PrivateRoute'
 
 function App() {
   return (
     <div className="App">
       <Switch>
-          <Route exact path="/" render={(routeProps) => <HomePage {...routeProps}/>}/>
-          <Route exact path="/about-us" render={(routeProps) => <AboutUsPage {...routeProps}/>}/>
+        <Route
+          exact
+          path="/"
+          render={(routeProps) => <HomePage {...routeProps} />}
+        />
+        <Route
+          exact
+          path="/about-us"
+          render={(routeProps) => <AboutUsPage {...routeProps} />}
+        />
+
+        <AnonRoute exact path="/signup" component={SignupPage} />
+        <AnonRoute exact path="/login" component={LoginPage} />
       </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
