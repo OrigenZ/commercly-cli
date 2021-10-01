ebbc1f73b5f45375696213979f59dcf7b7ffb1d6
lauraAlabau <arlaura3@gmail.com>
2021-10-01 14:25:59 +0200

    Implemented Search component 50%

--- src/components/Shop/ProductsList/ProductsList.js
+++ src/components/Shop/ProductsList/ProductsList.js
@@ -3,18 +3,16 @@ import ProductCard from "../../ProductCard/ProductCard";
 import "./ProductList.css";
 
 function ProductList(props) {
-  const { filteredProducts, products } = props;
+  const { filteredProducts, products, foundProducts } = props;
 
   console.log(
-    "filteredProducts",
-    filteredProducts.products,
-    "products",
-    products
+    "foundProducts",
+    foundProducts,
   );
 //TODO: Make a Card component
   return (
     <div className="products d-flex row justify-content-start ">
-      {!filteredProducts.products &&
+      {!filteredProducts.products && 
         products.map((product) => {
           return (
             <ProductCard
@@ -35,6 +33,30 @@ function ProductList(props) {
           );
         })}
       {filteredProducts.products && filteredProducts.products.length === 0 && (
+        <p>No hay productos</p> //TODO: Message
+      )}
+      {/*  */}
+      {!foundProducts &&
+        products.map((product) => {
+          return (
+            <ProductCard
+              key={product._id}
+              product={product}
+              isShop={true}
+            />
+          );
+        })}
+      {foundProducts &&
+        foundProducts.map((product) => {
+          return (
+            <ProductCard
+              key={product._id}
+              product={product}
+              isShop={true}
+            />
+          );
+        })}
+      {foundProducts && foundProducts.length === 0 && (
         <p>No hay productos</p> //TODO: Message
       )}
     </div>
