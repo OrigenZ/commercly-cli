580152e70c8d942c2a2a9e24c81b8d27bf43d162
Sherab Pereira <sherabpereira@gmail.com>
2021-10-02 15:31:00 +0200

    merge

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,20 +1,24 @@
-// TODO: Debería mostrar lista de productos, cantidad (editable) = eliminar producto 
+// TODO: Debería mostrar lista de productos, cantidad (editable) = eliminar producto
 
-import React, { useContext } from 'react';
-import { CartContext} from '../../common/context/Cart.context'
+import React, { useContext } from 'react'
+import { CartContext } from '../../common/context/Cart.context'
 
-function ShoppingCart(){
-    const { cart } = useContext(CartContext);
-    
-    return(
-        <div className='section container text-center'>
-        {cart && cart.products.map((product)=>(
-            
-            <p key={`${product._id}${Math.random()*1000}`}>id producto: {product.name}</p>
-            //TODO: for a better way to do this shit
-        ))} 
-        </div>
-    )
+function ShoppingCart() {
+  const { cart } = useContext(CartContext)
+
+  console.log(cart)
+
+  return (
+    <div className="section container text-center">
+      {cart &&
+        cart.products.map((product) => (
+          <p key={`${product._id}${Math.random() * 1000}`}>
+            id producto: {product.name}
+          </p>
+          //TODO: for a better way to do this shit
+        ))}
+    </div>
+  )
 }
 
 export default ShoppingCart
