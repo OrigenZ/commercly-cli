19571487eb452d895d60ea109d43086d9d8a3462
lauraAlabau <arlaura3@gmail.com>
2021-10-05 22:40:08 +0200

    CheckOutCart, LoginPage & SignupPage styles and forms

--- src/pages/MyAccount/LoginPage.js
+++ src/pages/MyAccount/LoginPage.js
@@ -1,75 +1,172 @@
-import { useState, useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 
-import { AuthContext } from '../../common/context/Auth.context'
-import axiosInstance from '../../common/http/index'
+import { AuthContext } from "../../common/context/Auth.context";
+import axiosInstance from "../../common/http/index";
+
+import Logo from "../../images/logo2.png";
+import "./LoginPage.css";
 
 const LoginPage = (props) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState(undefined)
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
 
-  const { logInUser } = useContext(AuthContext)
+  const { logInUser } = useContext(AuthContext);
 
-  const handleEmail = (e) => setEmail(e.target.value)
-  const handlePassword = (e) => setPassword(e.target.value)
+  const setField = (field, value) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+    // Check and see if errors , and remove them from the error object:
+    if (!!errors[field])
+      setErrors({
+        ...errors,
+        [field]: null,
+      });
+  };
 
   const handleLoginSubmit = (e) => {
-    e.preventDefault()
+    alert();
+    e.preventDefault();
+    const newErrors = findFormErrors();
 
-    const requestBody = { email, password }
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      const requestBody = { ...form };
 
-    axiosInstance
-      .post(`/api/auth/login`, requestBody)
-      .then((response) => {
-        const token = response.data.authToken
-        logInUser(token)
-        props.history.push('/') // TODO: redirect to account-details (comun en todos los perfiles)
-      })
-      .catch((error) => {
-        const errorDescription = error.response.data.message
-        setErrorMessage(errorDescription)
-      })
-  }
+      console.log(requestBody);
+
+      axiosInstance
+        .post(`/api/auth/login`, requestBody)
+        .then((response) => {
+          const token = response.data.authToken;
+          logInUser(token);
+          props.history.push("/"); // TODO: redirect to account-details (comun en todos los perfiles)
+        })
+        .catch((err) => {});
+    }
+  };
+
+  const findFormErrors = () => {
+    const { email, password } = form;
+    const newErrors = {};
+
+    const emailRegex = new RegExp(/^\S+@\S+\.\S+$/);
+    const passwordRegex = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/);
+
+    // email errors
+    if (!email || email === "") newErrors.email = "This field cannot be blank.";
+    else if (!emailRegex.test(email))
+      newErrors.email = "Please provide a valid email address.";
+
+    // password errors
+    if (!password || password === "")
+      newErrors.password = "This field cannot be blank.";
+    else if (!passwordRegex.test(password))
+      newErrors.password =
+        "Password must be 6 characters long and have one number, one lowercase and one uppercase letter.";
+
+    return newErrors;
+  };
 
   return (
-    <div className="section d-flex flex-column justify-content-center align-items-center ">
-      <div className="col-sm-12 col-md-6 col-lg-4 text-muted">
-        <h1>Login</h1>
+    <section
+      className="container d-flex flex-column justify-content-center align-items-center"
+      id="login"
+    >
+      <div className="login-wrapper">
+        <div className="text-center logo">
+          <Link to={"/my-account"} className="logolink">
+            <img
+              src={Logo}
+              alt="logo"
+              className="d-inline-block"
+              width="100"
+              height="100"
+            />
+          </Link>
+        </div>
 
-        <Form onSubmit={handleLoginSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control
-              type="text"
-              name="email"
-              value={email}
-              onChange={handleEmail}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              value={password}
-              onChange={handlePassword}
-            />
-          </Form.Group>
-          <Button variant="secondary" type="submit">
-            Login{' '}
-          </Button>
-        </Form>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <div className="mt-3">
-          <p>
-            Don't have an account yet? <Link to={'/signup'}> Sign Up</Link>
+        <div className="login-container">
+          <p className="text-center">
+            I'm an existing customer and would like to login.
           </p>
+
+          <Form className="text-center" onSubmit={handleLoginSubmit}>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  onChange={(e) => setField("email", e.target.value)}
+                  isInvalid={!!errors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  onChange={(e) => setField("password", e.target.value)}
+                  isInvalid={!!errors.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+
+            <div className="d-flex justify-content-between">
+              <div className="remember">
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Remember me" />
+                </Form.Group>
+              </div>
+
+              <div className="lost-password">
+                <a href="#!">Lost Password?</a>
+              </div>
+            </div>
+
+            <Button
+              variant="outline-secondary"
+              className="py-2 px-5 mt-4"
+              type="submit"
+            >
+              Login
+            </Button>
+          </Form>
+
+          <div className="text-center mb-3">
+            <p>
+              <strong>I'm a new customer and would like to register.</strong>
+            </p>
+            <Link
+              to={"/signup"}
+              className="btn btn-outline-secondary py-2 px-4"
+              id="create-account"
+            >
+              Create a New Account
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
