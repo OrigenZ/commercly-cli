8bd8a14957d45b063656b1c5319217634dc485aa
lauraAlabau <arlaura3@gmail.com>
2021-10-05 16:05:19 +0200

    Grid.js table for products and categories management

--- src/components/MyAccount/MyAdmin/ManageProducts/ProductsListAdmin/ProductsListAdmin.js
+++ src/components/MyAccount/MyAdmin/ManageProducts/ProductsListAdmin/ProductsListAdmin.js
@@ -11,10 +11,11 @@ import { _ } from "gridjs-react";
 import "gridjs/dist/theme/mermaid.css";
 
 import axiosInstance from "../../../../../common/http/index";
-import "./CategoriesListAdmin.css";
+import defaultImage from "../../../../../images/img-default.jpg";
+import './ProductsListAdmin.css'
 
-function CategoriesListAdmin(props) {
-  const [categories, setCategories] = useState([]);
+function ProductsListAdmin(props) {
+  const [products, setProducts] = useState([]);
   const wrapperRef = useRef(null);
 
   const storedToken = localStorage.getItem("authToken");
@@ -32,22 +33,18 @@ function CategoriesListAdmin(props) {
     })
       .then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            icon: "success",
-            text: `Category ${name} has been deleted.`,
-            showConfirmButton: false,
-          });
+          Swal.fire("Deleted!", `Product ${name} has been deleted.`, "success");
 
           axiosInstance
-            .delete(`/api/categories/${id}`, {
+            .delete(`/api/products/${id}`, {
               headers: { Authorization: `Bearer ${storedToken}` },
             })
             .then(() => {
-              const newCategories = categories.filter(
-                (category) => category._id !== id
+              const newProducts = products.filter(
+                (product) => product._id !== id
               );
-              setCategories([...newCategories]);
-              history.push("/my-account/admin/categories");
+              setProducts([...newProducts]);
+              history.push("/my-account/admin/products");
             });
         }
       })
@@ -66,27 +63,30 @@ function CategoriesListAdmin(props) {
     // If the token exists in the localStorage
     if (storedToken) {
       axiosInstance
-        .get(`/api/categories`, {
+        .get(`/api/products`, {
           headers: { Authorization: `Bearer ${storedToken}` },
         })
         .then((response) => {
-          const newData = response.data.map((category) => [
-            category.name,
-            category.description,
+          console.log("Category of proddss", response.data.products[0].category);
+          const newData = response.data.products.map((product) => [
+            product.sku,
+            product.name,
+            product.quantity,
+            product.category.name,
+            product.brand,
+            product.price,
             _(
               <div>
                 <Button
                   variant="outline-secondary"
                   onClick={() =>
-                    history.push(`/admin/category/edit/${category._id}`)
+                    history.push(`/admin/product/edit/${product._id}`)
                   }
                   className="btn btn-outline-secondary edit-btn w-100"
                 >
                   Edit
                 </Button>
-                <Button
-                  onClick={() => handleDelete(category._id, category.name)}
-                >
+                <Button onClick={() => handleDelete(product._id, product.name)}>
                   Delete
                 </Button>
               </div>
@@ -95,36 +95,40 @@ function CategoriesListAdmin(props) {
 
           new Grid({
             columns: [
+              "SKU",
               "Name",
-              { name: "Description", sort: false },
-              { name: "Actions", sort: false },
+              "Quantity",
+              "Category",
+              "Brand" ,
+              "Price" ,
+              { name:"Actions" , sort: false,},
             ],
             pagination: true,
             sort: true,
             search: {
-              selector: (cell, rowIndex, cellIndex) =>  cell
+              selector: (cell, rowIndex, cellIndex) => cell,
             },
             data: [...newData],
           }).render(wrapperRef.current);
 
-          setCategories(response.data);
+          setProducts(response.data);
         })
         .catch((err) => {});
     }
   }, []);
 
   return (
-  <>
-        <Link
-          to={`/admin/category/create`}
-          className="btn btn-outline-dark col-12"
-        >
-          Add Category
-        </Link>
+    <>
+      <Link
+        to={`/admin/product/create`}
+        className="btn btn-outline-dark col-12"
+      >
+        Add Product
+      </Link>
 
-        <div ref={wrapperRef} />
-  </>
+      <div ref={wrapperRef} />
+    </>
   );
 }
 
-export default CategoriesListAdmin;
+export default ProductsListAdmin;
