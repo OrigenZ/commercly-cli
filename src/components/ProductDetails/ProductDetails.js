82a504c05ac62ab16e1096f6dee735040c7477e7
Sherab Pereira <sherabpereira@gmail.com>
2021-10-07 20:14:58 +0200

    hide shopping button to admins

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../../common/context/Auth.context";
+import React from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { useParams } from 'react-router'
+import { Link } from 'react-router-dom'
+import { AuthContext } from '../../common/context/Auth.context'
 import Swal from 'sweetalert2/src/sweetalert2'
-import { CartContext } from "../../common/context/Cart.context";
-import axiosInstance from "../../common/http/index";
+import { CartContext } from '../../common/context/Cart.context'
+import axiosInstance from '../../common/http/index'
 
 import {
   Col,
@@ -15,46 +15,46 @@ import {
   Button,
   Form,
   Alert,
-} from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+} from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-import defaultImage from "../../images/img-default.jpg";
-import "./ProductDetails.css";
+import defaultImage from '../../images/img-default.jpg'
+import './ProductDetails.css'
 
 const ProductDetails = () => {
-  const { user } = useContext(AuthContext);
-  const [product, setProduct] = useState([]);
-  const [setErrorMessage] = useState(undefined);
-  const { cart, setCart, setCount } = useContext(CartContext);
+  const { user } = useContext(AuthContext)
+  const [product, setProduct] = useState([])
+  const [setErrorMessage] = useState(undefined)
+  const { cart, setCart, setCount } = useContext(CartContext)
 
-  const { id } = useParams();
+  const { id } = useParams()
 
-  const storedToken = localStorage.getItem("authToken");
+  const storedToken = localStorage.getItem('authToken')
 
   const handleCartItem = () => {
     if (product && cart) {
-      const body = { productId: product._id, cartId: cart._id };
+      const body = { productId: product._id, cartId: cart._id }
       axiosInstance
         .post(`/api/cart/add-item`, body, {
           headers: { Authorization: `Bearer ${storedToken}` },
         })
         .then((response) => {
-          setCart(response.data);
+          setCart(response.data)
           Swal.fire({
             icon: 'success',
             text: 'Product added to cart',
             showConfirmButton: false,
           })
-        });
+        })
     }
-  };
+  }
 
   useEffect(() => {
     if (cart) {
-      setCount(cart.products.length);
+      setCount(cart.products.length)
     }
-  });
+  })
 
   useEffect(() => {
     axiosInstance
@@ -62,14 +62,14 @@ const ProductDetails = () => {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => {
-        setProduct(response.data);
+        setProduct(response.data)
       })
       .catch((error) => {
-        const errorDescription = error.response.data.message;
-        setErrorMessage(errorDescription);
-      });
+        const errorDescription = error.response.data.message
+        setErrorMessage(errorDescription)
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
+  }, [id])
 
   return (
     <Container id="product-details">
@@ -89,7 +89,8 @@ const ProductDetails = () => {
           </div>
           <div>
             <p className="product-price">{product.totalPrice} €</p>
-            {user && (
+
+            {user && !user.isAdmin &&  (
               <Form onSubmit={(e) => e.preventDefault()}>
                 <Button
                   variant="outline-success"
@@ -115,7 +116,7 @@ const ProductDetails = () => {
         </Col>
       </Row>
     </Container>
-  );
-};
+  )
+}
 
-export default ProductDetails;
+export default ProductDetails
