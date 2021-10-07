163c4693640319fb68292c34f3a5c5a82e087dbf
Sherab Pereira <sherabpereira@gmail.com>
2021-10-07 15:39:31 +0200

    Added api url in production settings

--- src/common/http/index.js
+++ src/common/http/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 //Malakian were here
-const API_URL = "http://localhost:5005/";
+const API_URL = 'https://commercly-srv.herokuapp.com/';
 const axiosInstance = axios.create({
   baseURL: API_URL,
 });
 export default axiosInstance;
+
+// "http://localhost:5005/" 
