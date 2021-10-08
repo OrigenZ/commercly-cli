e1d3cceb2497ecc2c174579177e6ed5f28202862
Sherab Pereira <sherabpereira@gmail.com>
2021-10-08 03:49:31 +0200

    messages bio

--- src/common/http/index.js
+++ src/common/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //Malakian were here
-const API_URL = "http://localhost:5005/";
+const API_URL = "https://commercly-srv.herokuapp.com/";
 const axiosInstance = axios.create({
   baseURL: API_URL,
 });
