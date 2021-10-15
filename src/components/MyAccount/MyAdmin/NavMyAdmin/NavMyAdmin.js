622ee88c752a1d90d339d4d5f89b9671ab13b16a
lauraAlabau <arlaura3@gmail.com>
2021-10-15 11:05:25 +0200

    Modified styles in subnavbar (customer and admin).

--- src/components/MyAccount/MyAdmin/NavMyAdmin/NavMyAdmin.js
+++ src/components/MyAccount/MyAdmin/NavMyAdmin/NavMyAdmin.js
@@ -1,85 +1,77 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../../common/context/Auth.context";
 import { NavLink } from "react-router-dom";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./NavMyAdmin.css";
 
 const NavMyAdmin = () => {
   const { logOutUser } = useContext(AuthContext);
   return (
     <div id="subnav" className="border-bottom customer-menu px-3 text-muted">
-      <Container as={Row}>
-        <Col className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/dashboard"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          >
-            Dashboard
-          </NavLink>
-        </Col>
-        <Col className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/admin/products"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          >
-            Products
-          </NavLink>
-        </Col>
-        <Col className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/admin/categories"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          >
-            Categories
-          </NavLink>
-        </Col>
-        <Col  className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/admin/users"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          >
-            Users
-          </NavLink>
-        </Col>
-        <Col  className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/admin/orders"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          > 
-            Orders
-          </NavLink>
-        </Col>
-        <Col className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/account-details"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          >
-            Account details
-          </NavLink>
-        </Col>
-        <Col className="text-center">
-          <NavLink
-            exact
-            activeClassName="active"
-            onClick={logOutUser}
-            to="/"
-            className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
-          >
-            Logout
-          </NavLink>
-        </Col>
+      <Container className="subnav">
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/dashboard"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/admin/products"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Products
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/admin/categories"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Categories
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/admin/users"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Users
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/admin/orders"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Orders
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/account-details"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Account details
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          onClick={logOutUser}
+          to="/"
+          className="text-reset text-decoration-none text-uppercase button-navMyAdmin"
+        >
+          Logout
+        </NavLink>
       </Container>
     </div>
   );
