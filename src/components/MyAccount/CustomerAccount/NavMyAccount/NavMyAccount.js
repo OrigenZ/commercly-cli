eec5e004412e52d9991b4acde28f8ab35b716bf0
lauraAlabau <arlaura3@gmail.com>
2021-10-15 11:05:25 +0200

    Modified styles in subnavbar (customer and admin).

--- src/components/MyAccount/CustomerAccount/NavMyAccount/NavMyAccount.js
+++ src/components/MyAccount/CustomerAccount/NavMyAccount/NavMyAccount.js
@@ -1,68 +1,62 @@
-import { useContext } from 'react'
-import { AuthContext } from '../../../../common/context/Auth.context'
-import { NavLink } from 'react-router-dom'
-import { Col, Container, Row } from 'react-bootstrap'
-import './NavMyAccount.css'
+import { useContext } from "react";
+import { AuthContext } from "../../../../common/context/Auth.context";
+import { NavLink } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "./NavMyAccount.css";
 
 const NavMyAccount = () => {
-  const { logOutUser } = useContext(AuthContext)
+  const { logOutUser } = useContext(AuthContext);
   return (
     <div id="subnav" className="border-bottom customer-menu px-3 text-muted">
-      <Container as={Row}>
-        <Col xs={12} sm={4} lg={2} className="center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/dashboard"
-            className="text-reset text-decoration-none text-uppercase"
-          >
-            Dashboard
-          </NavLink>
-        </Col>
-        <Col xs={12} sm={4} lg={2} className="center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/customer/orders"
-            className="text-reset text-decoration-none text-uppercase"
-          >
-            Orders
-          </NavLink>
-        </Col>
-        <Col xs={12} sm={4} lg={2} className="center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/customer/address-list"
-            className="text-reset text-decoration-none text-uppercase"
-          >
-            Addresses
-          </NavLink>
-        </Col>
-        <Col xs={12} sm={4} lg={2} className="center">
-          <NavLink
-            exact
-            activeClassName="active"
-            to="/my-account/account-details"
-            className="text-reset text-decoration-none text-uppercase"
-          >
-            Account details
-          </NavLink>
-        </Col>
-        <Col xs={12} sm={4} lg={2} className="center">
-          <NavLink
-            exact
-            activeClassName="active"
-            onClick={logOutUser}
-            to="/"
-            className="text-reset text-decoration-none text-uppercase"
-          >
-            Logout
-          </NavLink>
-        </Col>
+      <Container className="subnav">
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/dashboard"
+          className="text-reset text-decoration-none text-uppercase button-navMyAccount"
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/customer/orders"
+          className="text-reset text-decoration-none text-uppercase button-navMyAccount"
+        >
+          Orders
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/customer/address-list"
+          className="text-reset text-decoration-none text-uppercase button-navMyAccount"
+        >
+          Addresses
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          to="/my-account/account-details"
+          className="text-reset text-decoration-none text-uppercase button-navMyAccount"
+        >
+          Account details
+        </NavLink>
+
+        <NavLink
+          exact
+          activeClassName="active"
+          onClick={logOutUser}
+          to="/"
+          className="text-reset text-decoration-none text-uppercase button-navMyAccount"
+        >
+          Logout
+        </NavLink>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default NavMyAccount
+export default NavMyAccount;
