8555817f48192f9f58eb36cd5d2054e13b73d838
Sherab Pereira <sherabpereira@gmail.com>
2021-12-12 18:55:17 +0100

    Fix: Admin/Customer dashboard links && styles

--- src/components/MyAccount/CustomerAccount/Dashboard/Dashboard.jsx
+++ src/components/MyAccount/CustomerAccount/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { AuthContext } from '../../../../common/context/Auth.context'
 
 import './Dashboard.css'
@@ -6,24 +7,29 @@ import './Dashboard.css'
 const Dashboard = () => {
   const { user, logOutUser } = useContext(AuthContext)
   return (
-    <div>
+    <div id="dashboard">
       <div className="dashboard-wrapper text-muted d-flex justify-content-center">
         <div className="d-flex flex-column justify-content-center align-items-center col-sm-12 col-md-8 col-lg-10">
           <p>
             Hello <strong>{user.username}</strong> (not{' '}
             <strong>{user.username}</strong>?
-            <a onClick={logOutUser} href="/">
+            <span onClick={logOutUser} className="logout">
+              {' '}
               Log out
-            </a>
+            </span>
             )
           </p>
           <p>
-            From your account dashboard you can manage your{' '}
-            <a href="/categories">products</a>, manage your{' '}
-            <a href="/products">categories</a>, and{' '}
-            <a href="/account-details">
+            From your account dashboard you can view your{' '}
+            <Link to={'/my-account/customer/orders'}> recent orders</Link>,
+            manage your{' '}
+            <Link to={'/my-account/customer/address-list'}>
+              shipping and billing addresses
+            </Link>
+            , and{' '}
+            <Link to={'/my-account/account-details'}>
               edit your password and account details
-            </a>
+            </Link>
             .
           </p>
         </div>
