5c476b47c77ffddf980674cc1fe320532cf27c34
Sherab Pereira <sherabpereira@gmail.com>
2021-12-12 19:00:14 +0100

    added API_URL const to .env

--- src/common/http/index.js
+++ src/common/http/index.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 //Malakian were here
-// const API_URL = "https://commercly-srv.herokuapp.com/";
-const API_URL = "http://localhost:5005/";
 const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.API_URL,
 });
 export default axiosInstance;
