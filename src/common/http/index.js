d99cb780b5baba813dc7a79ca7df7558e7422213
Sherab Pereira <sherabpereira@gmail.com>
2021-12-12 20:12:00 +0100

    fix: prefix env variables with REACT_APP_

--- src/common/http/index.js
+++ src/common/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //Malakian were here
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 export default axiosInstance;
