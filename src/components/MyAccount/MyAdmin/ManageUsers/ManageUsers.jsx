a0674eca0a869cd69f405a023b22daabc359053d
Sherab Pereira <sherabpereira@gmail.com>
2021-12-30 01:01:16 +0100

    fix: UsersListAdmin was calling api endpoint twice && code refactor && lift state up && ManageUsers async tasks refactor to async/await

--- src/components/MyAccount/MyAdmin/ManageUsers/ManageUsers.jsx
+++ src/components/MyAccount/MyAdmin/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
+
+import Swal from 'sweetalert2/src/sweetalert2'
 import axiosInstance from "../../../../common/http";
 import UsersListAdmin from "./UsersListAdmin/UsersListAdmin";
 
@@ -10,23 +12,54 @@ const ManageUsers = () => {
 
   const storedToken = localStorage.getItem("authToken");
 
-  useEffect(() => {
-    axiosInstance("/api/users", {
-      headers: { Authorization: `Bearer ${storedToken}` },
-    })
-      .then((response) => {
-        setUsers(response.data);
+  const getUsers = async () => {
+    try {
+      const response = await axiosInstance("/api/users", {
+        headers: { Authorization: `Bearer ${storedToken}` },
       })
-      .catch((err) => console.log(err.message));
-    //TODO: set proper error handling
+      setUsers(response.data);
+    } catch (err) { console.log(err.message) }
 
+  }
+
+  const handleDelete = async (id, name) => {
+    const input = await Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Delete',
+    })
+
+    if (input.isConfirmed) {
+      try {
+        Swal.fire('Deleted!', `User ${name} has been deleted.`, 'success')
+        await axiosInstance.delete(`/api/users/${id}`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        const newUsers = users.filter((user) => user._id !== id)
+        setUsers(newUsers)
+      } catch (err) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message,
+        })
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Container>
       <Row id="manage-users" xs={12}>
-        <UsersListAdmin users={users} setUsers={setUsers} />
+        <UsersListAdmin users={users} handleDelete={handleDelete} />
       </Row>
     </Container>
   )
--- src/components/MyAccount/MyAdmin/ManageUsers/UsersListAdmin/UsersListAdmin.jsx
+++ src/components/MyAccount/MyAdmin/ManageUsers/UsersListAdmin/UsersListAdmin.jsx
@@ -1,102 +1,33 @@
-import axiosInstance from '../../../../../common/http/index'
+
 import UserCard from '../../../../UserCard/UserCard'
 import { useState, useEffect } from 'react'
-import Swal from 'sweetalert2/src/sweetalert2'
 import ReactPaginate from 'react-paginate'
 import { Row, Col } from 'react-bootstrap'
 
 import './UsersListAdmin.css'
 
 const UsersListAdmin = (props) => {
-  const { users, setUsers } = props
+  const { users, handleDelete } = props
 
   const [offset, setOffset] = useState(0)
   const [data, setData] = useState([])
   const [perPage] = useState(5)
   const [pageCount, setPageCount] = useState(0)
 
-  const storedToken = localStorage.getItem('authToken')
 
-  const handlePageClick = (e) => {
-    const selectedPage = e.selected
-    setOffset(Math.ceil(selectedPage * perPage))
+  const handlePageClick = ({ selected }) => {
+    setOffset(Math.ceil(selected * perPage))
   }
 
-  const handleDelete = (id, name) => {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Delete',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire('Deleted!', `User ${name} has been deleted.`, 'success')
-
-        axiosInstance
-          .delete(`/api/users/${id}`, {
-            headers: { Authorization: `Bearer ${storedToken}` },
-          })
-          .then((response) => {
-            const newUsers = users.filter((user) => user._id !== id)
-            setUsers(newUsers)
-            getData()
-          })
-          .catch((err) => {
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Something went wrong!',
-            })
-          })
-      }
-    })
+  const getData = () => {
+    setPageCount(Math.ceil(users.length / perPage))
+    return users.slice(offset, offset + perPage)
   }
-
-  const getData = async () => {
-    try {
-      const response = await axiosInstance.get(`/api/users`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
-      const data = response.data
-
-      const slice = data.slice(offset, offset + perPage)
-
-      const postData = slice.map((user) => (
-        <UserCard key={user._id} userData={user} handleDelete={handleDelete} />
-      ))
-
-      setData(postData)
-      setPageCount(Math.ceil(data.length / perPage))
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const getUsers = async () => {
-    // If the token exists in the localStorage
-    if (storedToken) {
-      axiosInstance
-        .get(`/api/users`, {
-          headers: { Authorization: `Bearer ${storedToken}` },
-        })
-        .then((response) => {
-          setUsers(response.data)
-        })
-        .catch((err) => { })
-    }
-  }
-  useEffect(() => {
-    getData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [offset])
 
   useEffect(() => {
-    getUsers()
+    setData(getData())
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [offset, users, handleDelete])
 
   return (
     <Col id="users-list-admin" xs={12}>
@@ -116,15 +47,13 @@ const UsersListAdmin = (props) => {
         <Col xs={12} sm={3} lg={2} />
       </Row>
 
-      {!data.length ? (
-        <Row className="text-center p-5">
-          <p>No users found</p>
-        </Row>
-      ) : (
-        <Col xs={12}>
-          {data}
-        </Col>
-      )}
+      <Col xs={12}>
+        {data.map((user) => (
+          <UserCard key={user._id} userData={user} handleDelete={handleDelete} />
+        ))}
+
+        {!data.length && <Col xs={12} className="text-center p-5">No users found</Col>}
+      </Col>
 
       <Row className="pagination">
         <ReactPaginate
